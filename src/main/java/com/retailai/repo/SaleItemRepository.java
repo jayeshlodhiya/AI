e2506c94a74df497c0deedc54bb2a64af218be81
jayeshlodhiya <jglodhiya@gmail.com>
2025-08-27 15:02:56 +0530

    reformat code

--- src/main/java/com/retailai/repo/SaleItemRepository.java
+++ src/main/java/com/retailai/repo/SaleItemRepository.java
@@ -10,8 +10,10 @@ import java.time.LocalDateTime;
 
 public interface SaleItemRepository extends JpaRepository<SaleItem, Long> {
 
-  int countByProductIdAndSale_CreatedAtBetween(Long productId, LocalDateTime start, LocalDateTime end);
-  int countByProductId(Long productId);
-  @Query("SELECT AVG(s.price) FROM SaleItem s WHERE s.product.id = :productId")
-  Double findAveragePriceByProductId(Long productId);
+    int countByProductIdAndSale_CreatedAtBetween(Long productId, LocalDateTime start, LocalDateTime end);
+
+    int countByProductId(Long productId);
+
+    @Query("SELECT AVG(s.price) FROM SaleItem s WHERE s.product.id = :productId")
+    Double findAveragePriceByProductId(Long productId);
 }
